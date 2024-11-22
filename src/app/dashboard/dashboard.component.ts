cb2f0a8a03d91eb5ee597c246db7588acbab9c2b
BladimirGS <hugobladimirgs@gmail.com>
2024-11-21 20:37:59 -0600

    Agregar avatar al sidebar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { NgFor } from '@angular/common';
+import { UserService } from '../services/user.service'; 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, NgFor],
+  imports: [CommonModule, RouterLink, RouterOutlet], 
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   menuItems = [
-    { name: 'Usuarios', link: '/dashboard/users' },
-    { name: 'pokemon', link: '/dashboard/pokemon' },
-
+    { name: 'Users', link: '/dashboard/users' },
+    { name: 'Pokemon', link: '/dashboard/pokemon' },
   ];
+  currentUser: any = null; 
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.currentUser = this.userService.getCurrentUser();
+  }
+
+  logout(): void {
+    // this.userService.logout();
+    // this.currentUser = null; 
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -21,8 +22,23 @@ export class UserService {
     return this.getUsers().pipe(
       map(users => {
         const user = users.find(u => u.email === email && u.password === password);
+        if (user) {
+          // Guardar la información del usuario en localStorage
+          localStorage.setItem('currentUser', JSON.stringify(user));
+        }
         return !!user;
       })
     );
+  }
+
+  // Método para obtener el usuario actual desde localStorage
+  getCurrentUser(): any {
+    const user = localStorage.getItem('currentUser');
+    return user ? JSON.parse(user) : null;
+  }
+
+  // Método para cerrar sesión y eliminar la información del usuario
+  logout(): void {
+    localStorage.removeItem('currentUser');
   }
 }
